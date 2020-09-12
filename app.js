b3d15733f37b9f64c456eff4ad500914760084ce
mdakhib <mohamedakhib@gmail.com>
2020-09-12 17:55:17 +0530

    appJs initial set-up added

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express=require("express")
+const bodyParser=require("body-parser")
+const request=require("request")
+
 const app=express()
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-    res.send("the newsLetter is working")
+    res.sendFile(__dirname+"/signup.html")
+});
+
+app.post("/signup.html",(req,res)=>{
+    res.send("Thank you for Subcribe")
 })
 
 app.listen(3000,()=>{
